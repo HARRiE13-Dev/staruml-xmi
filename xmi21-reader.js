2082972c523625f4ef0104049bcbdb7d738ea264
Harrie <surachat@launchplatform.co.th>
2025-02-14 14:11:03 +0700

    fix 4

--- xmi21-reader.js
+++ xmi21-reader.js
@@ -354,8 +354,8 @@ function loadFromFile(filename) {
   // Parse XMI
   var parser = new DOMParser();
   var dom = parser.parseFromString(data, "text/xml");
-  var XMINode = dom.getElementsByTagName('xmi:XMI')[0]
-  console.log("v2 XMI Node: ", JSON.stringify(XMINode.attributes, null, 2));
+  var XMINode = dom.childNodes[0]; // dom.getElementsByTagName("XMI")[0];
+  console.log("XMI Node: ", JSON.stringify(XMINode.attributes, null, 2));
     
   // Read top-level elements
   var topLevelElements = [];
